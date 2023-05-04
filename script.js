3e14c629adf9ac4757dd7fc69b312a0bcc9bd74c
saba-bar95 <s.barbakadze95@gmail.com>
2023-05-04 14:36:01 +0400

    Add basic UI

--- script.js
+++ script.js
@@ -1,57 +1,68 @@
+const body = document.querySelector("body");
+const btns = document.querySelectorAll(".btn");
+
+function trimHumanSelection(str) {
+  return str[0].toUpperCase() + str.slice(1).toLowerCase();
+}
+
+function getComputerChoice() {
+  const computerNumber = Math.floor(Math.random() * 3 + 1);
+  if (computerNumber === 1) {
+    return "Rock";
+  } else if (computerNumber === 2) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+
 let humanWinCounter = 0;
 let computerWinCounter = 0;
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    // Play rounds based on human's input and computer's random choice
-    function playRound(humanSelection, computerSelection) {
-      humanSelection = trimHumanSelection(humanSelection);
-
-      if (humanSelection === computerSelection)
-        return "It's a tie, computer is as smart as human";
-
-      if (
-        (humanSelection === "Rock" && computerSelection === "Paper") ||
-        (humanSelection === "Paper" && computerSelection === "Scissors") ||
-        (humanSelection === "Scissors" && computerSelection === "Rock")
-      ) {
-        computerWinCounter++;
-        return `Computer Wins! ${computerSelection} beats ${humanSelection}`;
-      } else {
-        humanWinCounter++;
-        return `Human Wins! ${humanSelection} beats ${computerSelection}`;
-      }
-    }
-
-    function trimHumanSelection(string) {
-      return string[0].toUpperCase() + string.slice(1).toLowerCase();
-    }
-    function getComputerChoice() {
-      const computerNumber = Math.floor(Math.random() * 3 + 1);
-      if (computerNumber === 1) {
-        return "Rock";
-      } else if (computerNumber === 2) {
-        return "Paper";
-      } else {
-        return "Scissors";
-      }
-    }
-
-    const humanSelection = prompt(
-      "What is human's choice: rock, paper or scissors?"
-    );
-    const computerSelection = getComputerChoice();
-    console.log(playRound(humanSelection, computerSelection));
-  }
-
-  if (humanWinCounter > computerWinCounter) {
-    console.log(`Human won: ${humanWinCounter} to ${computerWinCounter}`);
-  } else if (humanWinCounter < computerWinCounter) {
-    console.log(`Computer won: ${computerWinCounter} to ${humanWinCounter}`);
+function playRound(humanSelection, computerSelection) {
+  humanSelection = trimHumanSelection(humanSelection);
+  if (humanSelection === computerSelection)
+    return `It's a tie, computer is as smart as human`;
+  if (
+    (humanSelection === "Rock" && computerSelection === "Paper") ||
+    (humanSelection === "Paper" && computerSelection === "Scissors") ||
+    (humanSelection === "Scissors" && computerSelection === "Rock")
+  ) {
+    computerWinCounter++;
+    return `Computer Wins! ${computerSelection} beats ${humanSelection}`;
   } else {
-    console.log(
-      `It's a tie. The score is ${humanWinCounter}:${computerWinCounter} `
-    );
+    humanWinCounter++;
+    return `Human Wins! ${humanSelection} beats ${computerSelection}`;
   }
 }
-game();
+
+function displayScore() {
+  if (humanWinCounter === 5)
+    return `Human reaches 5. Game over! Overal score ${humanWinCounter}:${computerWinCounter}`;
+  if (computerWinCounter === 5)
+    return `Computer reaches 5. Game over! Overal score ${computerWinCounter}:${humanWinCounter}`;
+
+  if (humanWinCounter > computerWinCounter) {
+    return `Running score: Human - ${humanWinCounter}, Computer - ${computerWinCounter}`;
+  } else if (humanWinCounter < computerWinCounter) {
+    return `Running score: Computer - ${computerWinCounter}, Human - ${humanWinCounter}`;
+  } else {
+    return `Running score: Computer - ${computerWinCounter}, Human - ${humanWinCounter}`;
+  }
+}
+
+btns.forEach((btn) =>
+  btn.addEventListener("click", function (e) {
+    if (humanWinCounter < 5 && computerWinCounter < 5) {
+      const humanSelection = e.target.textContent;
+      const computerSelection = getComputerChoice();
+      const div = document.createElement("div");
+      div.style.marginLeft = "100px";
+      const para = document.createElement("p");
+      div.textContent = playRound(humanSelection, computerSelection);
+      para.textContent = displayScore();
+      body.appendChild(div);
+      body.appendChild(para);
+    }
+  })
+);
